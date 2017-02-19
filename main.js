29d06667dce206c88d3b7282b72c972df770c84f
ThePlasmaGuy <theplasmaguy2403@gmail.com>
2017-02-18 20:09:04 -0700

    adjusted default height once again for more testing on actual driver station laptop

--- main.js
+++ main.js
@@ -31,7 +31,7 @@ function createWindow() {
 	// Create the browser window.
 	mainWindow = new BrowserWindow({
 		width: 1366,
-		height: 535,
+		height: 530,
 		// 1366x570 is a good standard height, but you may want to change this to fit your DriverStation's screen better.
 		// It's best if the dashboard takes up as much space as possible without covering the DriverStation application.
 		// The window is closed until the python server is ready
